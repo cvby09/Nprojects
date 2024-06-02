654783ef6360c2c5bf767c9c9bead8bf3813e481
cvby09 <zack.smith.w@gmail.com>
2024-06-02 16:38:15 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import struct
+from Cython.Build.BuildExecutable import build
 
 def check_32_bit_system():
     bit_size = struct.calcsize("P") * 8
@@ -9,8 +10,10 @@ def check_64_bit_system():
     return bit_size == 64
 
 if check_32_bit_system():
-    print("This is a 32-bit system.")
+    #print("This is a 32-bit system.")
+    build("hchk.py")
 elif check_64_bit_system():
-    print("This is a 64-bit system.")
+    #print("This is a 64-bit system.")
+    build("hchk.py")
 else:
     print("Unable to determine system architecture.")
