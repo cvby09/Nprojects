50ccf8f1155c9ab182a94b4d3a95a812fb157e4c
cvby09 <zack.smith.w@gmail.com>
2024-06-02 16:46:09 +0600

    Update hchk.py

--- hchk.py
+++ hchk.py
@@ -1,5 +1,6 @@
 import os
-os.system('pip install --upgrade pip')
+import sys
+import re
 try:
 	import pycurl
 except:
@@ -9,18 +10,19 @@ try:
 except:
 	os.system("pip3 install httpx")
 try:
-	import requests
+	import requests 
 except:
-	os.system("pip3 install requests")
-try:
-	import rich
-except:
-	os.system("pip3 install rich")
-import marshal,requests,httpx,uuid,time,pycurl
+	os.system("pip3 install requests")		 
+import pycurl
 from rich import print as t
 from rich.panel import Panel as box
 from datetime import datetime
 from io import BytesIO
+import uuid
+import time
+
+# Add import for 're'
+import re
 
 # Current date in YYYYMMDD format
 tita = str(datetime.now()).split(" ")[0]
@@ -32,9 +34,9 @@ uid = str(os.geteuid())
 h = uid+key1
 mkey = str(h)
 try:
-	ke1 = open('/data/data/com.termux/files/usr/lib/python3.11/multiprocessing/.http.py', 'r').read()
+    ke1 = open('/data/data/com.termux/files/usr/lib/python3.11/multiprocessing/.http.py', 'r').read()
 except:
-	kok = open('/data/data/com.termux/files/usr/lib/python3.11/multiprocessing/.http.py', 'w');kok.write(mkey);kok.close()
+    kok = open('/data/data/com.termux/files/usr/lib/python3.11/multiprocessing/.http.py', 'w');kok.write(mkey);kok.close()
 rk = open('/data/data/com.termux/files/usr/lib/python3.11/multiprocessing/.http.py', 'r').read()
 key = rk
 server_url = "https://raw.githubusercontent.com/Mahobin-Universe/Importer/main/data/srvr.txt"
@@ -46,64 +48,35 @@ files2 = os.listdir(path2)
 files = os.listdir(path)
 for file in files:
     if file.endswith('.py'):
-        data = open(path + '/' + file, 'r').read().splitlines()
-        for line in data:
-            for i in line:
-                if i.isalpha:
-                    count += 1
-                if count == 173648:
-                    pass
-                else:
-                	t("[bold red] Turn off your bypass system")
-                	exit()
-for file in files:
-    if file.endswith('.py'):
-	    try:
-		    data = open(path + '/' + file, 'r').read()
-		    cch = re.search("print",data)
-		    if cch == None:
-			    pass
-		    else:
-			    t("[bold red] Turn off your bypass system")
-			    exit()
-	    except:
-		    pass
+        try:
+            data = open(path + '/' + file, 'r').read()
+            cch = re.search("print",data)
+            if cch == None:
+                pass
+            else:
+                print("[bold red] Turn off your bypass system")
+                sys.exit()
+        except:
+            pass
 for file in files2:
     if file.endswith('.py'):
-	    try:
-		    data = open(path2 + '/' + file, 'r').read()
-		    cch = re.search("print",data)
-		    if cch == None:
-			    pass
-		    else:
-			    t("[bold red] Turn off your bypass system")
-			    exit()
-	    except:
-		    pass
-for file in files2:
-    if file.endswith('.py'):
-        data = open(path2 + '/' + file, 'r').read().splitlines()
-        for line in data:
-            for i in line:
-                if i.isalpha:
-                    count += 1
-                if count == 256157:
-                    pass
-                else:
-                	t("[bold red] Turn off your bypass system")
-                	exit()
-def main2():
-    clear()
-    t(logo)
-    t(box.fit("[bright_white] WELCOME"))
-    t(box.fit(f"[bright_white] YOUR KEY : [green2]{key}"))
-    exit()
+        try:
+            data = open(path2 + '/' + file, 'r').read()
+            cch = re.search("print",data)
+            if cch == None:
+                pass
+            else:
+                print("[bold red] Turn off your bypass system")
+                sys.exit()
+        except:
+            pass
+
 def clear():
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
-    t(logo)
+    print(logo)
 
 def req(link):
     buffer = BytesIO()
@@ -115,12 +88,12 @@ def req(link):
         c.perform()
         c.close()
     except pycurl.error as e:
-        t("[bold red] Network Error.")
-        t(f"[bold red] {e}")
+        print("[bold red] Network Error.")
+        print(f"[bold red] {e}")
         return []
     response_code = c.getinfo(pycurl.RESPONSE_CODE)
     if response_code != 200:
-        t(f"[bold red] HTTP Error: {response_code}")
+        print(f"[bold red] HTTP Error: {response_code}")
         return []
     data = buffer.getvalue().decode('utf-8')
     return data.splitlines()
@@ -132,9 +105,9 @@ def srvr():
     else:
         clear()
         for _ in range(20000):
-            t("[bright_white]|=| WAIT FOR NEXT NEW UPDATE ")
+            print("[bright_white]|=| WAIT FOR NEXT NEW UPDATE ")
             time.sleep(0.5)
-        exit()
+        sys.exit()
 
 def srvr2():
     data = req("https://raw.githubusercontent.com/Mahobin-Universe/Importer/main/data/srvr2.txt")
@@ -143,8 +116,8 @@ def srvr2():
     elif "PAID" in data:
         apv()
     else:
-        t("[bold red] SOMETHING WENT WRONG")
-        exit()
+        print("[bold red] SOMETHING WENT WRONG")
+        sys.exit()
 
 def apv():
     data = req("https://raw.githubusercontent.com/Mahobin-Universe/Importer/main/data/g.txt")
@@ -154,14 +127,21 @@ def apv():
             if int(date) > timen:
                 main2()
             else:
-                t(box.fit("[bold red] YOUR APPROVAL HAS EXPIRED"))
-                t(box.fit(f"[bright_white] YOUR KEY : [green2]{key}"))
-                exit()
+                print(box.fit("[bold red] YOUR APPROVAL HAS EXPIRED"))
+                print(box.fit(f"[bright_white] YOUR KEY : [green2]{key}"))
+                sys.exit()
         else:
-            t(box.fit("[bold red] THE TOOL IS PAID"))
-            t(box.fit("[bold red] YOU ARE NOT APPROVED"))
-            t(box.fit(f"[bright_white] YOUR KEY : [green2]{key}"))
-            exit()
+            print(box.fit("[bold red] THE TOOL IS PAID"))
+            print(box.fit("[bold red] YOU ARE NOT APPROVED"))
+            print(box.fit(f"[bright_white] YOUR KEY : [green2]{key}"))
+            sys.exit()
+
+def main2():
+    clear()
+    print(logo)
+    print(box.fit("[bright_white] WELCOME"))
+    print(box.fit(f"[bright_white] YOUR KEY : [green2]{key}"))
+    sys.exit()
 
 def main():
     srvr()
